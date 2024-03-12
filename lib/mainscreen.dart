e624b5a66827498ac08166dab2ed8dcba4d3db87
eoox77 <eoox77@gmail.com>
2024-03-13 00:23:17 +0900

    .

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -8,19 +8,49 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  TextEditingController idController = TextEditingController();
+  String msg = "이 곳에 입력값이 업데이트 됩니다.";
+  ValueNotifier<int> counter = ValueNotifier<int>(0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("MainScreen"),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text("Hi $index"),
-          );
-        },
-        itemCount: 10,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              controller: idController,
+              decoration: InputDecoration(labelText: "ID를 입력하세요"),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // setState(() {
+              //   if (idController.text.toString().length != 0) {
+              //     msg = idController.text.toString();
+              //   }
+              //   else{
+              //     msg = "이 곳에 입력값이 업데이트 됩니다.";
+              //   }
+              // });
+              counter.value ++;
+              print(idController.text.toString());
+            },
+            child: Text("ID 입력값 가져오기"),
+          ),
+          Text(
+            msg,
+            style: TextStyle(fontSize: 20),
+          ),
+          ValueListenableBuilder<int>(valueListenable: counter, builder: (context, value, child) {
+            return Text("Count: $value");
+          },)
+        ],
       ),
     );
   }
