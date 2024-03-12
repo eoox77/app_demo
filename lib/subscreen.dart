730f9fe30e0589eacb966d113389306dbc71e5af
eoox77 <eoox77@gmail.com>
2024-03-12 19:21:00 +0900

    ..

---
+++ lib/subscreen.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class SubScreen extends StatelessWidget {
+  const SubScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SubScreen"),
+      ),
+    );
+  }
+}
